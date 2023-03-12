2e85b1fc4f66ab17c69f8aacff5bd9a08b66eceb
Joshua Yang <jyangftw@gmail.com>
2023-03-11 23:00:30 -0800

    Good Progress. Link Animation, Svg placement

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,14 +1,16 @@
 import React from 'react'
+import '/Rectangle.svg';
 
 export const Hero = () => {
   return (
     <div className='Hero'>
       <div className='left-hero'>
-        <img src="/hero-vector.svg" alt="vector" />
+          <img classname="vector" src="/hero-vector.svg" alt="vector" />
         <button className="vector-button" >Sign Up!</button>
       </div>
       <div className='right-hero'>
         <img className="Hero-Image" src="src/GEC/Wiliam.jpg"/>
+        <img className='svg-rectangle' src='/Rectangle.svg' />
       </div>
     </div>
   )
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,12 +5,12 @@ import '/src/styles/Navbar.css'
 export const Navbar = () => {
   return (
     <nav>
-      <img src="/logo.svg" className="logo" alt="GEC logo" />
-      <nav>
-        <h1>About</h1>
-        <h1>Mission</h1>
-        <button className="nav-button">Sign Up</button>
-      </nav>
+      <a href="#Home"><img src="/logo.svg" className="logo" alt="GEC logo" /></a>
+      <div className="nav-links">
+        <a href="#About" className="normal-links">About</a>
+        <a href="#Mission" className="normal-links">Mission</a>
+        <a><button className="nav-button">Contact</button></a>
+      </div>
     </nav>
   )
 }
