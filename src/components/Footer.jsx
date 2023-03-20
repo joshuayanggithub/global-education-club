6b6dd01ce4c4ff84137affe3d264cf3bf5745986
Joshua Yang <jyangftw@gmail.com>
2023-03-19 18:17:46 -0700

    Preliminary Footer With Icons

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
 import '/src/styles/Footer.css'
+import {BsInstagram} from 'react-icons/bs'
+import {SiLinktree} from 'react-icons/si'
+import {BsGithub} from 'react-icons/bs'
+
 
 export const Footer = () => {
   return (
-    <section className='footer'>
+    <footer className='footer'>
       <h1 className='footer-text'>©2023 Designed and Created By Joshua Yang</h1>
-    </section>
+      <div className='icons'>
+        <a href='https://www.instagram.com/qlsglobaleducation/'><BsInstagram size={20} color="black" /></a>
+        <a href='https://linktr.ee/qlsglobaleducation'><SiLinktree size={20} color="black" /></a>
+        <a href='https://github.com/joshuayanggithub/global-education-club'><BsGithub size={20} color="black" /></a>
+      </div>
+    </footer>
   )
 }
