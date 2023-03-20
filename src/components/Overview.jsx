312a955995b6dcc3568b991ad781d89f4c2eba67
Joshua Yang <jyangftw@gmail.com>
2023-03-19 18:08:23 -0700

    Added icons to overview

--- src/components/Overview.jsx
+++ src/components/Overview.jsx
@@ -1,4 +1,8 @@
 import React from 'react'
+import '/src/styles/Overview.css'
+import {FcReading} from 'react-icons/fc'
+import {FaHandsHelping} from 'react-icons/fa'
+import {BsFillHouseHeartFill} from 'react-icons/bs'
 
 export const Overview = () => {
   return (
@@ -8,18 +12,21 @@ export const Overview = () => {
         <p>
           Every week, our high school volunteers, currently on Saturdays from 7:45 to 8:25 PM PST, we read books from our little library to young students K-5.
         </p>
+        <FcReading size={60}/>
       </div>
       <div className='tutoring'>
         <h1>Tutoring</h1>
         <p>
           Every week, our high school volunteers, currently on Wednesdays during high-school club time from 3:25 to 4:10 PM, we tutor kids on various subjects or help them out with schoolwork!
         </p>
+        <FaHandsHelping size={60}  color="#f3d945"/>
       </div>
       <div className='projects'>
         <h1>Other Projects</h1>
         <p>
           We are always participating in local outreach or partnering with other non-profit organizations to support our mission of empowering education (ALP)!
         </p>
+        <BsFillHouseHeartFill size={60} color="#f34b45"/>
       </div>
     </div>
   )
