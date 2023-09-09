a6542d89b2b46abfa6ce8f339b39851e2f4feaad
Joshua Yang <jyangftw@gmail.com>
2023-09-09 10:56:08 -0700

    Updated 23/24 Year

--- src/components/Overview.jsx
+++ src/components/Overview.jsx
@@ -1,57 +1,101 @@
-import React from 'react'
-import '/src/styles/Overview.css'
-import {FcReading} from 'react-icons/fc'
-import {FaHandsHelping} from 'react-icons/fa'
-import {BsFillHouseHeartFill} from 'react-icons/bs'
+import React from "react";
+import "/src/styles/Overview.css";
+import { FcReading } from "react-icons/fc";
+import { FaHandsHelping } from "react-icons/fa";
+import { BsFillHouseHeartFill } from "react-icons/bs";
 
 export const Overview = () => {
   return (
-      <section id='Overview' className='overview'>
-      
-        <div class="wave">
-          <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
-              <path d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z" class="shape-fill"></path>
-          </svg>
-        </div>
-
-        <h1>Our Programs</h1>
-        <p>An overview </p>
-
-        <svg class="plane" width="378" height="311" viewBox="0 0 378 311" fill="none" xmlns="http://www.w3.org/2000/svg">
-        <path d="M327.058 153.408L320.467 171.632M327.058 153.408L346.969 174.704C348.342 175.25 349.715 174.34 350.127 172.702L374.981 117.186C375.393 115.366 374.707 113.546 373.471 113L290.12 142.487C289.708 144.307 290.394 146.127 291.63 146.673L320.467 150.132M327.058 153.408L374.26 115.048L320.467 150.132M320.467 171.632L331.989 158.681M320.467 171.632V150.132" stroke="#EAE197" stroke-width="4" stroke-linejoin="round"/>
-        <path d="M325.811 162.176C244.974 303.318 144.657 76.8866 93.4379 46.5182C42.2191 16.1497 122.225 363.506 46.5485 289.117C21.5288 264.523 -2.87183 221.651 -2.87183 221.651" stroke="black" stroke-dasharray="4 4"/>
+    <section id="Overview" className="overview">
+      <div class="wave">
+        <svg
+          data-name="Layer 1"
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 1200 120"
+          preserveAspectRatio="none"
+        >
+          <path
+            d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z"
+            class="shape-fill"
+          ></path>
         </svg>
-
-        <div className='three-column'>
-          <div className='reading'>
-            <h1>Reading</h1>
-            <p>
-              Every week, our QLS/GEC volunteers, currently on Saturdays from 7:45 to 8:25 PM PST, read books from our little library to young students K-5.
-            </p>
-            <FcReading className='overview-icon'/>
-          </div>
-          <div className='tutoring'>
-            <h1>Tutoring</h1>
-            <p>
-              Every week, our Quarry Lane/GEC volunteers, currently on Wednesdays during high-school club time from 3:25 to 4:10 PM, tutor kids on various subjects & help them out with schoolwork!
-            </p>
-            <FaHandsHelping className='overview-icon' color="#f3d945" />
-          </div>
-          <div className='projects'>
-            <h1>Other Projects</h1>
-            <p>
-              We are always participating in local outreach or partnering with other non-profit organizations/ clubs to support our mission of empowering education such as ALP!
-            </p>
-            <BsFillHouseHeartFill className='overview-icon' color="#f34b45"/>
-          </div>
-        </div>
-
-        <div class="wave-bottom">
-          <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
-              <path d="M0,0V7.23C0,65.52,268.63,112.77,600,112.77S1200,65.52,1200,7.23V0Z" class="shape-fill"></path>
-          </svg>
       </div>
 
-      </section>
-  )
-}
+      <h1>Our Programs</h1>
+      <p>An overview </p>
+
+      <svg
+        class="plane"
+        width="378"
+        height="311"
+        viewBox="0 0 378 311"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          d="M327.058 153.408L320.467 171.632M327.058 153.408L346.969 174.704C348.342 175.25 349.715 174.34 350.127 172.702L374.981 117.186C375.393 115.366 374.707 113.546 373.471 113L290.12 142.487C289.708 144.307 290.394 146.127 291.63 146.673L320.467 150.132M327.058 153.408L374.26 115.048L320.467 150.132M320.467 171.632L331.989 158.681M320.467 171.632V150.132"
+          stroke="#EAE197"
+          stroke-width="4"
+          stroke-linejoin="round"
+        />
+        <path
+          d="M325.811 162.176C244.974 303.318 144.657 76.8866 93.4379 46.5182C42.2191 16.1497 122.225 363.506 46.5485 289.117C21.5288 264.523 -2.87183 221.651 -2.87183 221.651"
+          stroke="black"
+          stroke-dasharray="4 4"
+        />
+      </svg>
+
+      <div className="three-column">
+        <a href="#Reading" className="overview-links">
+          <div className="reading">
+            <h1>Reading</h1>
+            <p>
+              Every week, our Quarry Lane/GEC volunteers, currently on Thursdays
+              from 4:15 to 5:15 PM, read books and stories to young students
+              K-5.
+            </p>
+            <FcReading className="overview-icon" />
+          </div>
+        </a>
+
+        <a href="#Tutoring" className="overview-links">
+          <div className="tutoring">
+            <h1>Tutoring</h1>
+            <p>
+              Every week, our Quarry Lane/GEC volunteers, currently on Thursdays
+              from 4:15 to 5:15 PM (combined with reading), tutor kids on
+              various subjects & help them out with schoolwork!
+            </p>
+            <FaHandsHelping className="overview-icon" color="#f3d945" />
+          </div>
+        </a>
+
+        <a href="#Mission" className="overview-links">
+          <div className="projects">
+            <h1>Other Projects</h1>
+            <p>
+              We are always participating in local outreach or partnering with
+              other non-profit organizations/ clubs to support our mission of
+              empowering education such as ALP!
+            </p>
+            <BsFillHouseHeartFill className="overview-icon" color="#f34b45" />
+          </div>
+        </a>
+      </div>
+
+      <div class="wave-bottom">
+        <svg
+          data-name="Layer 1"
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 1200 120"
+          preserveAspectRatio="none"
+        >
+          <path
+            d="M0,0V7.23C0,65.52,268.63,112.77,600,112.77S1200,65.52,1200,7.23V0Z"
+            class="shape-fill"
+          ></path>
+        </svg>
+      </div>
+    </section>
+  );
+};
--- src/components/Tutoring.jsx
+++ src/components/Tutoring.jsx
@@ -1,29 +1,43 @@
-import React from 'react'
-import '/src/styles/Tutoring.css'
+import React from "react";
+import "/src/styles/Tutoring.css";
 
 export const Tutoring = () => {
   return (
-    <section id='Tutoring' className='Tutoring'>
-      <div className='tutoring-info'>
-        <h1>Our <h1 className='emph'>Tutoring</h1> <strong>Program</strong></h1>
+    <section id="Tutoring" className="Tutoring">
+      <div className="tutoring-info">
+        <h1>
+          Our <h1 className="emph">Tutoring</h1> <strong>Program</strong>
+        </h1>
         <h2>Why Choose Us?</h2>
         <p>
-          As high schoolers, we have all shared the struggles of falling behind in schoolwork or understanding everything that is taught in class without having the support or time to catch up to the teacher. It's nice for us to help our younger Quarry Lane classmates for a change, so that we can give the help or simply company they want/need!
-          </p>
+          As high schoolers, we have all shared the struggles of falling behind
+          in schoolwork or understanding everything that is taught in class.
+          It's nice for us to help our younger Quarry Lane classmates for a
+          change, so that we can give the help or simply company they want/need!
+        </p>
         <h2>How We Operate</h2>
         <p>
-          Every week on a specific day on a school year, our volunteers meet and find tutorees from the lower school building/area. This 2022/23 school year, our tutoring sessions take place on Wednesdays from 3:25 QLS HS club time to 4:10 PM in the lower-school cafeteria. If the time doesn't work, we also have tutors who schedule times outside of this interval with the parents.
+          Every week on a specific day on a school year, our volunteers meet and
+          find tutorees from the lower school building/area.{" "}
+          <b>
+            <em>
+              This 2023/24 school year, we will meet for tutoring sessions on
+              Thursdays 4:15-5:15 in the lower school.
+            </em>
+          </b>
+          If the time doesn't work, we also have tutors who schedule times
+          outside of this interval with the parents.
         </p>
       </div>
-      <div className='picture-container'>
-        <div className='tutoring-pictures'>
-          <img src='/Srinitha_Tutoring.jpg'/>
-          <div className='tutoring-caption'>
+      <div className="picture-container">
+        <div className="tutoring-pictures">
+          <img src="/Srinitha_Tutoring.jpg" />
+          <div className="tutoring-caption">
             <h3>2022/23</h3>
             <h3>Tutoring in Progress!</h3>
           </div>
         </div>
       </div>
     </section>
-  )
-}
+  );
+};
